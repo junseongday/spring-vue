128dc2029d1056a55b97aeb45f99e9f66a0a4fa8
junseongday <junseongday@gmail.com>
2020-07-13 07:36:24 +0900

    log print

--- src/main/java/com/example/springvue/ErrorController.java
+++ src/main/java/com/example/springvue/ErrorController.java
@@ -1,13 +1,20 @@
 package com.example.springvue;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
     @GetMapping("/error")
-    public String redirect() {
-        System.out.println("redirect111");
+    public String redirect(HttpServletRequest request) {
+
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        HttpStatus httpStatus = HttpStatus.valueOf(Integer.valueOf(status.toString()));
+        System.out.println("httpStatus : "+httpStatus.toString());
         return "error.html";
     }
 
