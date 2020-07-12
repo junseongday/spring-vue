a03caaa138d6f2f8b53eab5d3047ae546e34a277
junseongday <junseongday@gmail.com>
2020-07-13 00:08:21 +0900

    editt

--- src/main/java/com/example/springvue/ErrorController.java
+++ src/main/java/com/example/springvue/ErrorController.java
@@ -5,15 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
-    @Override
-    public String getErrorPath() {
-        System.out.println("getErrorPath");
-        return "/error";
-    }
-
     @GetMapping("/error")
     public String redirect() {
-        System.out.println("redirect");
-        return "/error.html";
+        System.out.println("redirect111");
+        return "error.html";
+    }
+
+    @Override
+    public String getErrorPath() {
+        System.out.println("!!!!");
+        return "null";
     }
 }
